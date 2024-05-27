af4f945619896f1e7dd4265a1941d1abf287e68d
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-05-27 14:24:26 +0600

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 2: Задайте строку, содержащую латинские буквы 
+в обоих регистрах. Сформируйте строку, 
+в которой все заглавные буквы заменены на строчные. */
+
+string CharArrayToString(char[] arr)
+{
+    string res = ""; 
+    foreach (char c in arr)
+    {
+        if (char.IsUpper(c))
+        {
+            res += char.ToLower(c);
+        }
+        else
+        {
+            // Добавление строчной буквы или символа без изменений
+            res += c;
+        }
+    }
+    return res;
+}
+
+char[] array = { 'A', 'b', 'C', 'D', 'e', 'f' };
+string result = CharArrayToString(array);
+Console.WriteLine(result); 
