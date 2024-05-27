6ac38e0890c76bf91e0a0d4065bc9d47dea13ecf
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-05-27 14:07:56 +0600

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 1: Задайте двумерный массив символов (тип char [,]). 
+Создать строку из символов этого массива. */
+
+string CharArrayToString(char[,] arr)
+{
+    string res = "";
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            res += arr[i, j];
+        }
+    }
+    return res.ToString();
+}
+
+    char[,] charArray = {
+            {'A', 'B', 'C'},
+            {'D', 'E', 'F'},
+            {'G', 'H', 'I'}
+        };
+
+
+    string result = CharArrayToString(charArray);
+    Console.WriteLine(result);
+
+    
