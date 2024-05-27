e1646f6bfd85c23b7c80ac46680123dfa07881e5
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-05-27 14:46:13 +0600

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 3: Задайте произвольную строку. Выясните, 
+является ли она палиндромом. */
+
+
+
+bool IsPalindrome(string input)
+{
+    int left = 0;
+    int right = input.Length - 1;
+
+    while (left < right)
+    {
+        if (input[left] != input[right])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+string input = " abba ";
+
+bool isPalindrome = IsPalindrome(input);
+Console.WriteLine(isPalindrome ? "Yes" : "No");
